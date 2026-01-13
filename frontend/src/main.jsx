ce753e231e43df02d5dfa09ed01aa452f0c42c4b
Yash <yashmadake93@gmail.com>
2026-01-13 16:54:50 +0530

    Implement role-based login and signup flow

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './contexts/AuthContext'
+import { LanguageProvider } from './contexts/LanguageContext'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <LanguageProvider>  {/* Add Outer Provider */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </LanguageProvider>
+  </StrictMode>,
 )
